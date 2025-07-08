c818efa391141734d8147a7cd21db7331c45cfa1
FearCleevan <jonathan.mauring17@gmail.com>
2025-07-08 20:30:32 +0800

    first commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PatientsModule } from './patients/patients.module';
 import { VisitsModule } from './visits/visits.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/patient-visit'),
+    ConfigModule.forRoot(), // Add this line
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost/patient-visit'),
     PatientsModule,
     VisitsModule,
   ],
